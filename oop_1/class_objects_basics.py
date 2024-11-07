d7d98690af81cd8325d421c44154765ffa6f8f7c
“Vrushali <vrushaliapotdar@gmail.com>
2024-11-07 16:11:13 +0530

    oop_1

---
+++ oop_1/class_objects_basics.py
@@ -0,0 +1,12 @@
+# Basic concepts of class and object in python
+
+# creating class
+
+class Student:
+    name = "vrushali potdar"
+
+
+#creating object (instance)
+
+s1 = Student()
+print(s1.name)
